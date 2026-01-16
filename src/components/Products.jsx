360906d1313562eae9bc8a1ab44ff1034d79af7d
MBSkipper <trawler22@gmail.com>
2026-01-16 20:21:50 +0000

    dynamic categories in a dropdown button added

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -3,9 +3,11 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
 
 function Products(props) {
     const [products, setProducts] = useState([ ])
+    const [categories, setCategories] = useState([ ])
 
     useEffect(() => {
         fetchProducts()
@@ -16,33 +18,81 @@ function Products(props) {
             const res = await fetch('https://dummyjson.com/products')
             const data = await res.json()
             setProducts(data.products)
+
+            {/*the following creates the categories dynamically using the data in the API */}
+            let productCategories = data.products.map(p => p.category) 
+            const uniqueCategories = [ ]
+            productCategories.forEach(c => {
+                if(!uniqueCategories.includes(c)){
+                    uniqueCategories.push(c)
+                }
+            })
+            setCategories(uniqueCategories)
         } catch (error) {
             console.log(error)
         }
     }
 
     return (
-        <Row className='mt-5'>
-            {/*note ternary operator below to show products if there is anything in the array or to show 'Sorry ..etc' if nothing */}
-            {products.length ? products.map(product => ( 
-                <Col>
-                    <Card style={{ width: '18rem', margin:'1em auto'}}> {/*margin style added to ensure centered text and card for sm screen */}
-                            <Card.Img variant="top" src={product.images[0]} /> {/*selects first image in array of images in API data*/} 
-                            <Card.Body>
-                                <Card.Title>{product.title}</Card.Title>
-                                <Card.Text>
-                                {product.price}
-                                </Card.Text>
-                                <Button variant="warning" className="me-1">Add to cart</Button>
-                                <Button variant="info" className="me-1">Checkout</Button>
-                            </Card.Body>
-                    </Card>
-                </Col>
-            )) : (
-                <p className='lead'>Sorry, no products in stock</p>
-            )}
-        </Row>
+        <>
+            <div className='d-flex justify-content-end'>
+                <Form.Select style={{width:'10em'}}>
+                    <option>All</option>
+                    {categories.map((category, index) => (
+                        <option key={index}>
+                            {category[0].toUpperCase() + category.slice(1)} {/*categories are lower case in the API this code adds a capital letter to the word and adds it to the slice of the remaining word */}
+                        </option>
+                    ))}
+                </Form.Select>
+            </div>
+
+            <Row className='mt-5'>
+                {/*note ternary operator below to show products if there is anything in the array or to show 'Sorry ..etc' if nothing */}
+                {products.length ? products.map(product => ( 
+                    <Col> key={product.id}    {/*When using map need a unique key for each product so product.id is used */}
+                        <Card style={{ width: '18rem', margin:'1em auto'}}> {/*margin style added to ensure centered text and card for sm screen */}
+                                <Card.Img variant="top" src={product.images[0]} /> {/*selects first image in array of images in API data*/} 
+                                <Card.Body>
+                                    <Card.Title style={{minHeight:'48px'}}>{product.title}</Card.Title>
+                                    <Card.Text>
+                                    {product.price}
+                                    </Card.Text>
+                                    <Button variant="warning" className="me-1">Add to cart</Button>
+                                    <Button variant="info" className="me-1">Checkout</Button>
+                                </Card.Body>
+                        </Card>
+                    </Col>
+                )) : (
+                    <p className='lead'>Sorry, no products in stock</p>
+                )}
+            </Row>
+        </>
     );
 }
 
 export default Products;
+
+
+{/** NOTES
+Categories of items 
+found by code on line starting const categories ... followed by console.log(categories).  Review in console and pasted below
+
+async function fetchProducts() {
+        try {
+            const res = await fetch('https://dummyjson.com/products')
+            const data = await res.json()
+            setProducts(data.products)
+
+            const categories = data.products.map(p => p.category)
+            console.log(categories)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+
+    
+    Categories
+    (30) ['beauty', 'beauty', 'beauty', 'beauty', 'beauty', 'fragrances', 'fragrances', 'fragrances', 'fragrances', 'fragrances', 'furniture', 'furniture', 'furniture', 'furniture', 'furniture', 'groceries', 'groceries', 'groceries', 'groceries', 'groceries', 'groceries', 'groceries', 'groceries', 'groceries', 'groceries', 'groceries', 'groceries', 'groceries', 'groceries', 'groceries']
+    
+    */}
