0fe215fad081fce4cc602cc5acbb0e4972c9400c
MBSkipper <trawler22@gmail.com>
2026-01-16 21:03:27 +0000

    dynamic product filtering by category

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -7,17 +7,29 @@ import Form from 'react-bootstrap/Form';
 
 function Products(props) {
     const [products, setProducts] = useState([ ])
+    const [filteredProducts, setFilteredProducts] = useState([ ])
     const [categories, setCategories] = useState([ ])
+    const [category, setCategory] = useState('all')
 
     useEffect(() => {
         fetchProducts()
     }, [ ])
+
+    useEffect(() => {
+        if(category == 'all'){
+            setFilteredProducts(products)
+        } else {
+            setFilteredProducts(products.filter(p => p.category == category))
+        }
+    }, [category])
+    
 
     async function fetchProducts() {
         try {
             const res = await fetch('https://dummyjson.com/products')
             const data = await res.json()
             setProducts(data.products)
+            setFilteredProducts(data.products)
 
             {/*the following creates the categories dynamically using the data in the API */}
             let productCategories = data.products.map(p => p.category) 
@@ -36,10 +48,14 @@ function Products(props) {
     return (
         <>
             <div className='d-flex justify-content-end'>
-                <Form.Select style={{width:'10em'}}>
-                    <option>All</option>
+                <Form.Select 
+                    value= {category}
+                    onChange={(e) => setCategory(e.target.value)}
+                    style={{width:'10em'}}
+                >
+                    <option value='all'>All</option>
                     {categories.map((category, index) => (
-                        <option key={index}>
+                        <option key={index} value={category}>
                             {category[0].toUpperCase() + category.slice(1)} {/*categories are lower case in the API this code adds a capital letter to the word and adds it to the slice of the remaining word */}
                         </option>
                     ))}
@@ -48,14 +64,14 @@ function Products(props) {
 
             <Row className='mt-5'>
                 {/*note ternary operator below to show products if there is anything in the array or to show 'Sorry ..etc' if nothing */}
-                {products.length ? products.map(product => ( 
-                    <Col> key={product.id}    {/*When using map need a unique key for each product so product.id is used */}
+                {filteredProducts.length ? filteredProducts.map(product => ( 
+                    <Col key={product.id}>   {/*When using map need a unique key for each product so product.id is used */}
                         <Card style={{ width: '18rem', margin:'1em auto'}}> {/*margin style added to ensure centered text and card for sm screen */}
                                 <Card.Img variant="top" src={product.images[0]} /> {/*selects first image in array of images in API data*/} 
                                 <Card.Body>
                                     <Card.Title style={{minHeight:'48px'}}>{product.title}</Card.Title>
                                     <Card.Text>
-                                    {product.price}
+                                    £{product.price}
                                     </Card.Text>
                                     <Button variant="warning" className="me-1">Add to cart</Button>
                                     <Button variant="info" className="me-1">Checkout</Button>
